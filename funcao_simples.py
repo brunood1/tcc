ceaae945907282d1600808f42e482cfc5d196715
brunood1 <ibruuno@outlook.com.br>
2025-02-17 16:27:48 -0300

    xx

--- funcao_simples.py
+++ funcao_simples.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-1.681,1.87474,100)
-y = (-(0.76 * x - 0.65)**4 - (0.76 * x - 0.65)**3 + (0.76 * x - 0.65)**2 - (0.76 * x - 0.65) + 1) * 0.7
+# x = np.linspace(-1.681,1.87474,100)
+# y = (-(0.76 * x - 0.65)**4 - (0.76 * x - 0.65)**3 + (0.76 * x - 0.65)**2 - (0.76 * x - 0.65) + 1) * 0.7
+x = np.linspace(-5.65,5.65,10000)
+y = -(1/16) * x**2 + 2
 
 z = np.zeros_like(y)
 
-n = 2
+n = 3
 for k in range(n * 2**n + 1):
     for i in range(len(y)):
         if k != n * 2**n:
@@ -21,5 +23,13 @@ for k in range(n * 2**n + 1):
                 print(f"f(x) = {k} in {x[i]}")
 
 plt.plot(x,y)
-plt.scatter(x,z,marker='.')
+plt.plot(x,z)
 plt.show()
+
+# table = np.zeros((len(x),2))
+# for i in range(len(x)):
+#     table[i,0] = x[i]
+#     table[i,1] = z[i]
+    
+# for i in range(len(x)):
+#     print(f"{table[i,0]:.3f}, {table[i,1]:.3f}")
